6ab457a8d9e1b105a8502a732f65ac7d3525dcf6
harrison <hday2002@gmail.com>
2026-03-17 20:33:28 -0500

    Added HandleMovement(), GetMouseWorldPos(), and LookAt() to Player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,24 +15,35 @@ public class Player : MonoBehaviour
         inputActions.Enable();
 
         mainCamera = Camera.main;
+
+        LookAt(new Vector3(0f, 0f, 0f));
     }
 
     void Update() 
     {
-        // Movement
+        HandleMovement();
+        LookAt(GetMouseWorldPos());
+    }
+
+    void HandleMovement()
+    {
         Vector3 pos = transform.position;
         float yMovement = input.VerticalMovement.ReadValue<float>();
         float xMovement = input.HorizontalMovement.ReadValue<float>();
         pos.y += (speed * yMovement) * Time.deltaTime;
         pos.x += (speed * xMovement) * Time.deltaTime;
         transform.position = pos;
+    }
 
-        // Object to Mouse Vector
+    Vector3 GetMouseWorldPos()
+    {
         Vector2 mouseScreenPos = Mouse.current.position.ReadValue();
-        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(new Vector3(mouseScreenPos.x, mouseScreenPos.y, 10f));
-        Vector3 direction = (mouseWorldPos - transform.position).normalized;
-
-        // Make Player look at Mouse
+        return Camera.main.ScreenToWorldPoint(new Vector3(mouseScreenPos.x, mouseScreenPos.y, 10f));
+    }
+    
+    void LookAt(Vector3 pos)
+    {
+        Vector3 direction = (pos - transform.position).normalized;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0, 0, angle);
     }
